2dc32d48a530ba2ea927cea8db1dc7e6acded6c8
fanqingyu <timo_quiet@163.com>
2021-04-18 01:33:29 +0800

    build: 新增跨域库

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const Koa = require('koa')
 const parser = require('koa-bodyparser')
+const cors = require("koa2-cors"); // 导入跨域模块
 const catchError = require('./middlewares/exception')
 const InitManager = require('./core/init')
 const app = new Koa()
+const allowOrigins = [
+  "http://localhost:5001"
+]
+app.use(cors({
+  origin: function(ctx) {
+    if (allowOrigins.includes(ctx.header.origin)) {
+      return ctx.header.origin;
+    }
+    return false;
+  },
+  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+  maxAge: 5,
+  credentials: true,
+  withCredentials:true,
+  allowMethods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'],
+  allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+}))
 app.use(catchError)
 app.use(parser())
 InitManager.initCore(app)
 
-app.listen(4000)
+app.listen(5000)
--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,11 +1,20 @@
 const Router = require('koa-router')
+const axios = require('axios')
 const router = new Router()
 const { PositiveIntegerValidator } = require('../../validators/validator')
-router.post('/v1/:id/classic/latest', (ctx, next) => {
-  console.log(ctx.params);
-  
-  const v = new PositiveIntegerValidator().validate(ctx)
-  ctx.body = 'success'
+const loginData = {
+  "account": "admin",
+  "password": "OUpGQLbUgJCBtgSAErZ7SsglyzPEPWnXgL8zFiXlSDsRbo6NbvXOFntEpUdcQIAl+QTji+hwRTu54/rc9r5d8PW43V5A6h0WXTcNuvIh0XcCtz4QJlQUbohxvtpFwfZs08wpsqg/RgI/cPldwWcttRkS8YzaYBrkIZaX9qNLros="
+}
+router.post('/api/admin/login', async (ctx, next) => {
+  console.log('params', ctx.params);
+  console.log('body', ctx.request.body);
+  console.log('query', ctx.request.query);
+  console.log('header', ctx.request.header);
+  const data = await axios.post('http://49.233.163.136:3002/admin/login', loginData)
+  ctx.body = data.data
+  // const v = new PositiveIntegerValidator().validate(ctx)
+  // ctx.body = 'success'
 })
 
 module.exports = router
